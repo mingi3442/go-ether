ef314de2d5caa7d05694749205ceb86e797f8573
Lee MinGi <mingi3442@gmail.com>
2023-02-16 19:02:30 +0900

    Get UncleBlock Data & Transactions Data in Blocks

--- blocklistner.go
+++ blocklistner.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "context"
-  // "encoding/hex"
+  "encoding/hex"
   "fmt"
   "github.com/ethereum/go-ethereum/core/types"
   "github.com/ethereum/go-ethereum/ethclient"
@@ -42,6 +42,36 @@ func BlockListener() error {
       fmt.Println("Block Nonce : ", block.Nonce())
       fmt.Println("Total Transactions : ", len(block.Transactions()))
       fmt.Println("----------------------------------------------------------------------")
+
+      if len(block.Uncles()) > 0 {
+        for _, uncle := range block.Uncles() {
+          uncleFee := float64((uncle.Number.Uint64()+8-block.Number().Uint64())*2) / 8.0 // Uncle Block Fee 계산
+          fmt.Println("****************************************************************")
+          fmt.Println("Uncle Block Length : ", len(block.Uncles()))
+          fmt.Println("Uncle Miner Address : ", uncle.Coinbase.Hex())
+          fmt.Println("Uncle Block Number : ", uncle.Number.Uint64())
+          fmt.Println("Uncle Block Reward : ", uncleFee)
+          fmt.Println("****************************************************************")
+        }
+      }
+      for _, tx := range block.Transactions() {
+        fmt.Println("#####################################################################")
+        fmt.Println("Transaction Hash : ", tx.Hash().Hex())
+        fmt.Println("Transfer Values : ", tx.Value().String(), "wei") // Transaction의 value값은 wei단위로 나옴
+        if tx.To() != nil {
+          //Transcation에 To가 있을 경우
+          fmt.Println("To Address : ", tx.To())
+        } else {
+          //Transcation에 To가 없을 경우 CA
+          fmt.Println("Create Contract ")
+        }
+        fmt.Println("Transaction Nonce : ", tx.Nonce())
+        fmt.Println("Transaction Gas Limit : ", tx.Gas()) // 예상 Gas Limit
+        fmt.Println("Transaction GasFeeCap : ", tx.GasFeeCap().Uint64())
+        fmt.Println("Transaction GasTipCap : ", tx.GasTipCap().Uint64())
+        fmt.Println("Transcation Input Data : ", hex.EncodeToString(tx.Data())) // Input Data는 인코딩이 필요
+        fmt.Println("#####################################################################")
+      }
     }
   }
 

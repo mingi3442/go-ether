402579c1edef866567319d8588066941937ce335
Lee MinGi <mingi3442@gmail.com>
2023-02-16 14:40:46 +0900

    Fix import Error

--- blocklistner.go
+++ blocklistner.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-  "context"
-  "encoding/hex"
-  "fmt"
-  "github.com/ethereum/go-ethereum/core/types"
+  // "context"
+  // "encoding/hex"
+  // "github.com/ethereum/go-ethereum/core/types"
   "github.com/ethereum/go-ethereum/ethclient"
 )
 
 func BlockListener() error {
-  client, err := etherclient.Dial("wss://mainnet.infura.io/ws/v3/adf81b4ad1e04a768dc24131023d50de")
+  _, err := ethclient.Dial("wss://mainnet.infura.io/ws/v3/adf81b4ad1e04a768dc24131023d50de")
+  // fmt.Println("aa")
+  return err
 }
